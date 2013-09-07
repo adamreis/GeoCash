46473f84a1c1637f7205e4e4159d24a2e94b7bd7
Adam Reis <ahr2127@columbia.edu>
2013-09-07 13:25:05 -0400

    commit before integrating kaitlins new changes

--- GeoCash.py
+++ GeoCash.py
@@ -1,12 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from geocash_constants import APP_SECRET, FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET 
+import requests
+import urllib
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
-	return render_template('index.html')
+foursq_access_base_url = 'https://foursquare.com/oauth2/access_token/?'
+redirect_uri = 'https://geocash.herokuapp.com/home/'
 
-@app.route('/push/', methods=['post'])
+@app.route('/')
+def index():
+	if '4sqid' in session:
+		return redirect(url_for('home'))
+	else:
+	# return render_template('index.html')
+
+@app.route('/newuser/')
+def new_user():
+	code = request.args.get('code', '')
+	print 'CODE: '+str(code)
+	args = {'client_id':FOURSQUARE_CLIENT_ID, 
+		'client_secret':FOURSQUARE_CLIENT_SECRET, 
+		   'grant_type':'authorization_code', 
+		 'redirect_uri':redirect_uri,
+		 'code':code}
+
+	token = json.loads(requests.get(foursq_access_base_url+urllib.urlencode(args)))['access_token']
+
+	return 'TOKEN: '+token
+
+
+@app.route('/home/', methods=['GET'])
+def home():
+	if '4sqid' not in session:
+		return redirect(url_for('index'))
+
+
+
+
+@app.route('/push/', methods=['POST'])
 def dummy_push():
 	print 'request:'
 	print request.form
@@ -14,6 +46,9 @@ def dummy_push():
 
 
 # app.wsgi_app = ProxyFix(app.wsgi_app)
+
+app.secret_key = APP_SECRET
+
 
 if __name__ == '__main__':
 	app.run(port=7777,debug=True)
