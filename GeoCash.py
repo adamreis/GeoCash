bd0403df743e19cf5cf302c492edfd2f4d8d6689
Adam Reis <ahr2127@columbia.edu>
2013-09-07 09:35:28 -0400

    installed gunicorn, ready for heroku test

--- GeoCash.py
+++ GeoCash.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+	return render_template('index.html')
+
+
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+	app.run(port=7777,debug=True)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GeoCash:app
