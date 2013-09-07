1bbf285ff9b4762befe15fc8e8b3b8f33b3b2ab7
Adam Reis <ahr2127@columbia.edu>
2013-09-07 10:02:40 -0400

    imported request

--- GeoCash.py
+++ GeoCash.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def main_page():
 @app.route('/push/', methods=['post'])
 def dummy_push():
 	print 'request:'
-	# print request
+	print request
 	return '200 OK'
 
 
