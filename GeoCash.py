952aa9a22c60ed84860de67ee2cab463c900070f
Adam Reis <ahr2127@columbia.edu>
2013-09-07 16:12:10 -0400

    fuckkk

--- GeoCash.py
+++ GeoCash.py
@@ -49,7 +49,7 @@ def new_user():
 	print 'ACCESS TOKEN: '+user_access_token
 	# return foursq_get_user_id_base_url+'oauth_token='+user_access_token+'&v=20130907'
 
-	return requests.get(foursq_get_user_id_base_url+'oauth_token='+user_access_token+'&v=20130907').json()
+	return str(requests.get(foursq_get_user_id_base_url+'oauth_token='+user_access_token+'&v=20130907').json())
 
 
 
