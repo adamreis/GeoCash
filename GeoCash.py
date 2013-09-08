4f4b58af7e00a06518e018758896119426eaa46a
Adam Reis <ahr2127@columbia.edu>
2013-09-08 00:57:57 -0400

    fuckkkkkkkkkkKkkkkkkKKkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- GeoCash.py
+++ GeoCash.py
@@ -182,8 +182,8 @@ def send_notification_email(sender_name, recipient_first_name, recipient_email, 
 	venue_name = venue_response['user']['name']
     #return venue_response
 
-	text = "Hi " + toName + ",\n" +
-		"Your friend " + sender_name + " sent you a GeoCash gift! It's waiting for you \
+	text = "Hi " + toName + ",\n Your \
+		friend " + sender_name + " sent you a GeoCash gift! It's waiting for you \
 		at " venue_name + ".\n" + sender_name + ": " + note + "\n\n" + "Go to http://geoca.sh \
 		to authenticate GeoCash with Foursquare so you can claim the payment on Venmo when you \
 		check in there.\n Authenticating will let GeoCash know when you check in at a place \
