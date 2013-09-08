443d8cbc1afbece49c270524e377b2d9ae030f39
Adam Reis <ahr2127@columbia.edu>
2013-09-07 23:10:22 -0400

    committing for kaitlin6

--- GeoCash.py
+++ GeoCash.py
@@ -132,7 +132,7 @@ def add_venue():
 	session['chosen_venue']=venue_id
 	return redirect(url_for('home'))
 
-@app.route('/add_pending_payment/', methods=['POST'])
+@app.route('/add_pending_payment/', methods=['GET'])
 def add_pending_payment():
 	note = request.args.get('note', '')
 	amount = request.args.get('amount', '')
