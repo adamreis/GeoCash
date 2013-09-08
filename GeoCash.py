d45d9d4b5639c190f9f525d6630d7ce141380369
Adam Reis <ahr2127@columbia.edu>
2013-09-08 02:50:48 -0400

    fuckkkkkkkkkkKkkkkkkKKkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- GeoCash.py
+++ GeoCash.py
@@ -281,7 +281,8 @@ def dummy_push():
 	print 'request:'
 	recip_id = json.loads(request.form['user'])['id']
 	venue_id = json.loads(request.form['checkin'])['venue']['id']
-	print 'recip id:'+recip_id
+	print 'recip id: '+recip_id
+	print 'venue id: '+venue_id
 	gift_to_process = pending_gift_collection.find_one({'recipient_id':recip_id,'venue_id':venue_id})
 
 	print 'gift to process: '+gift_to_process
